99209cb98af17dc4618f7ff9ddb9483d134783a3
André Arruda <andre.todimo@gmail.com>
2022-11-30 17:39:00 -0400

    using javascript

---
+++ principal.js
@@ -0,0 +1,7 @@
+var titulo = document.getElementsByTagName('h2');
+
+for(let i = 0;i < titulo.length; i++)
+{
+    titulo.item(i).style.color = 'black';
+    //titulo.item(i).style.fontSize = '32px'
+}
